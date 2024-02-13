7a54707cb7f53d4207c3ac1794842613f98fcb76
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-02-13 19:32:40 +0900

    test : CalculateNumber add Domain 테스트 코드 추가

--- src/test/java/domain/CalculateNumberTest.java
+++ src/test/java/domain/CalculateNumberTest.java
@@ -3,6 +3,7 @@ package domain;
 import org.example.domain.CalculateNumber;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class CalculateNumberTest {
 
@@ -15,7 +16,28 @@ public class CalculateNumberTest {
         CalculateNumber calculateNumber = new CalculateNumber(value);
 
         //Then
-        Assertions.assertInstanceOf(CalculateNumber.class,calculateNumber);
         Assertions.assertEquals(calculateNumber.getValue(),value);
     }
+    @Test
+    void 정수를_통해_도메인을_생성한다(){
+        //Given
+        Integer value = 1000;
+
+        //When
+        CalculateNumber calculateNumber = new CalculateNumber(value);
+
+        //Then
+        Assertions.assertInstanceOf(CalculateNumber.class,calculateNumber);
+    }
+    @Test
+    void 계산숫자_두개를_통해_계산된_계산숫자를_반환한다(){
+        CalculateNumber calculateNumber1 = new CalculateNumber(100);
+        CalculateNumber calculateNumber2 = new CalculateNumber(200);
+
+        CalculateNumber calculatedNumber = calculateNumber1.add(calculateNumber2);
+
+        Assertions.assertEquals(calculatedNumber.getValue(),300);
+    }
+
+
 }
